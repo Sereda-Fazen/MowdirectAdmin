8d9ea9c1de82185faa6627994a72d3fa81c98452
Sereda-Fazen <fazen7@mail.ru>
2016-07-04 16:35:13 +0300

    admin test-2

--- tests/acceptance/00_TestCest.php
+++ tests/acceptance/00_TestCest.php
@@ -12,10 +12,25 @@ class TestCest
         $I->loginAdminPanel('testing', 'Da1mat1an5');
         $manageCategories->goToManageCategory();
         $manageCategories->createCategory('Test category');
+    }
+    function T832EditCategory(Step\Acceptance\AdminPanelLoginSteps $I, \Page\MagentoManageCategories $manageCategories)
+    {
         $manageCategories->editCategory('Test category');
+    }
+    function T832MoveAboveCategory(Step\Acceptance\AdminPanelLoginSteps $I, \Page\MagentoManageCategories $manageCategories)
+    {
         $manageCategories->aboveCategory();
+    }
+    function T832MoveBelowCategory(Step\Acceptance\AdminPanelLoginSteps $I, \Page\MagentoManageCategories $manageCategories)
+    {
         $manageCategories->belowCategory();
+    }
+    function T832IntoCategory(Step\Acceptance\AdminPanelLoginSteps $I, \Page\MagentoManageCategories $manageCategories)
+    {
         $manageCategories->intoSibling();
+    }
+    function T832DeleteCategory(Step\Acceptance\AdminPanelLoginSteps $I, \Page\MagentoManageCategories $manageCategories)
+    {
         $manageCategories->deleteCategory();
 
     }
